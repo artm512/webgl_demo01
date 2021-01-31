2b8acbe7584dfe87c4bbef37168e6de53e8b82a1
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-01-31 16:00:36 +0900

    feat: main実装 未完成

--- public/main.js
+++ public/main.js
@@ -5,4 +5,55 @@ if(!gl) {
   throw new Error('WebGL not supported');
 }
 
-alert("Everything's peachy hear with WebGL");
+// 1. vertexData = [...]
+
+// 2. create Buffer
+// 3. load vertexDate into buffer
+
+// 4. create vertex shader
+// 5. create fragment shader
+// 6. create program
+// 7. attach shaders to program
+
+// 8. enable vertex attributes
+
+// 9.draw
+
+const vertexData = [
+  0, 1, 0,
+  1, -1, 0,
+  -1, -1, 0,
+];
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, `
+attribute vec3 position;
+void main() {
+  gl_Position = vec4(position, 1)
+}
+`);
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader,`
+void main() {
+  gl_FragColor = vec4(1, 0, 0, 1);
+}
+`);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program, 'position');
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.useProgram(program);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
