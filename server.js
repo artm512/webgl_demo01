0e262ca425b94e6750acbb0de14359f727aa5ce4
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-01-31 15:13:28 +0900

    feat: server.js修正

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+// express モジュールのインスタンス作成
 const express = require('express');
-
 const app = express();
+// パス指定用モジュール
+const path = require('path');
 
-app.use(express.static('public'));
+// 8080番ポートで待ちうける
+app.listen(3000, () => {
+  console.log('Running at Port 3000...\n-----\nURL: http://localhost:3000\n-----');
+});
 
-app.listen(3000);
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'public')));
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+  res.sendStatus(404);
+});
